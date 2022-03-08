bc562f939a54e1d9a2b13d0d7928729f76c2b301
kishore <you@example.com>
2022-03-09 04:30:27 +0530

    first commit

---
+++ connection.js
@@ -0,0 +1,9 @@
+const mongoose= require("mongoose")
+exports.connect=()=>{
+    try{
+        mongoose.connect("mongodb+srv://Kishoref1:kishorec@cluster0.rru3b.mongodb.net/amazon")
+        console.log("connection Success")
+    }catch(err){
+        console.log(err)
+    }
+}
---
+++ models/user.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+    username:{
+        type:String,
+        minLength: 5,
+        required: true
+    },
+    email:{
+        type: String,
+        required: true
+    },
+    phone:{
+        type: Number,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true,
+        minLength: 5,
+    },
+    password: {
+        type: String,
+        required: true,
+        minLength: 5,
+    }
+  
+});
+
+const User=mongoose.model('User', userSchema, 'user');
+module.exports = User;
