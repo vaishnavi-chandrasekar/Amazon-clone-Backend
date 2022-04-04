6a95a2891155cafa6991171254fb12ddc73ce19d
vaishnavi-chandrasekar <92520546+vaishnavi-chandrasekar@users.noreply.github.com>
2022-04-05 00:12:51 +0530

    Update user.js

--- models/user.js
+++ models/user.js
@@ -4,7 +4,7 @@ const Schema = mongoose.Schema;
 const userSchema = new Schema({
     username:{
         type:String,
-        minLength: 5,
+        minLength: 3,
         required: true
     },
     email:{
